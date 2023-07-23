cfd4181dbde07b291e6c2f48f8f262849bf0d630
alecoad <andrewlecoad@gmail.com>
2023-07-22 22:35:52 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// GLOBAL VARIABLES
+// The div with profile information
+const overviewDiv = document.querySelector(".overview");
+// GitHub username
+const username = "alecoad";
+
+// FETCH API JSON DATA
